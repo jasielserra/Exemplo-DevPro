18a37d98aa11d8016e851e3d0d59477a4a56f723
jasiel <jasiel_serra@hotmail.com>
2021-08-30 00:22:33 -0300

    Exercicio 2 com strings da lista Python Brasil

---
+++ matematica/InvertendoStrings.py
@@ -0,0 +1,9 @@
+nome = 'Jasiel de Cristo Serra'.upper()
+
+nome_invertido_por_letras = ''.join(reversed(nome))
+
+nome_invertido_por_palavras = ' '.join(reversed(nome.split()))
+
+print(f'Nome com letras em maiúsculo: {nome}')
+print(f'Nome com letras em maiúsculo invertido por letras: {nome_invertido_por_letras}')
+print(f'Nome com letras em maisúculo invertido por palavras: {nome_invertido_por_palavras}')
