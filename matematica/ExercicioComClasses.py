fe1bf495eb28c6e0a522c53ebe9c2073c31e03e6
jasiel <jasiel_serra@hotmail.com>
2021-09-04 22:02:13 -0300

    Comparando objetos de uma mesma classe.

---
+++ matematica/ExercicioComClasses.py
@@ -0,0 +1,8 @@
+class CirculoPerfeito:
+    pass
+
+circulo_primeiro = CirculoPerfeito()
+circulo_segundo = CirculoPerfeito()
+
+print(type(circulo_primeiro))
+print(circulo_primeiro is circulo_segundo)
