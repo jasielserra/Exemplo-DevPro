0e770f42bb5821f0ae8d9d3af07a9335374380f9
jasiel <jasiel_serra@hotmail.com>
2021-09-04 22:19:00 -0300

    Definindo metodos e atributos em classes.

--- matematica/ExercicioComClasses.py
+++ matematica/ExercicioComClasses.py
@@ -1,8 +1,21 @@
 class CirculoPerfeito:
-    pass
+    def __init__(self):
+        self.cor = 'Azul'
+        self.circunferencia = 4
+        self.material = 'Papel'
+
+    def mostra_cor(self):
+        return self.cor
+
+    def trocar_cor(self, cor):
+        self.cor = cor
 
 circulo_primeiro = CirculoPerfeito()
 circulo_segundo = CirculoPerfeito()
+circulo_segundo.trocar_cor('Amarelo')
 
 print(type(circulo_primeiro))
 print(circulo_primeiro is circulo_segundo)
+print(id(circulo_primeiro), circulo_primeiro.mostra_cor())
+print(id(circulo_segundo), circulo_segundo.mostra_cor())
+print(circulo_primeiro.cor, circulo_segundo.cor)
