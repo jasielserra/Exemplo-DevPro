f3a54062313ca006dc611cdcf57918c347186642
jasiel <jasiel_serra@hotmail.com>
2021-09-04 23:04:34 -0300

    Calculando area do quadrado.

--- matematica/ExercicioComClasses.py
+++ matematica/ExercicioComClasses.py
@@ -19,3 +19,13 @@ print(circulo_primeiro is circulo_segundo)
 print(id(circulo_primeiro), circulo_primeiro.mostra_cor())
 print(id(circulo_segundo), circulo_segundo.mostra_cor())
 print(circulo_primeiro.cor, circulo_segundo.cor)
+
+class Quadrado:
+    def __init__(self, lado = 1):
+        self.lado = lado
+
+    def calcular_area(self):
+        return self.lado ** 2
+
+quadrado = Quadrado(4)
+print(quadrado.lado, quadrado.calcular_area())
