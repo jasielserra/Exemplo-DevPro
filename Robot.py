b38157c527011a0c61b8139684aeb5a5776955d9
jasiel Serra <jasiel_serra@hotmail.com>
2021-11-02 20:11:06 -0300

    Implementaando metodos de movimento do Robot.

--- Robot.py
+++ Robot.py
@@ -3,6 +3,18 @@ class Robot():
         self.x = x
         self.y = y
 
+    def move_up(self):
+        self.y = self.y + 1
+
+    def move_down(self):
+        self.y = self.y - 1
+
+    def move_right(self):
+        self.x = self.x + 1
+
+    def move_left(self):
+        self.x = self.x - 1
+
 class Robot3D(Robot):
     def __init__(self, x, y, z):
         super(Robot3D, self).__init__(x,y)
