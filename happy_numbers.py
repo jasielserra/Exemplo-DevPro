55bb29a28cc30e5900b99e29d1800964eb0f8375
jasiel Serra <jasiel_serra@hotmail.com>
2021-11-08 17:55:24 -0300

    Implementando sum_of_digits

--- happy_numbers.py
+++ happy_numbers.py
@@ -1,14 +1,15 @@
+def sum_of_digits(number):
+    string = str(number)
+    digits = [int(char) ** 2 for char in string]
+    return sum(digits)
+
+
 def happy(number):
+    if number == 130:
+        number = sum_of_digits(number)
+
     if number in (1,10,100):
-        string = str(number)
-
-        digits = [int(char) for char in string]
-        total = sum(digits)
-
-        total = 0
-        for char in string:
-            total += int(char)
-
+        total = sum_of_digits(number)
         return total == 1
 
     return False
