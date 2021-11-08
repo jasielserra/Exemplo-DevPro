04305e63cce21ffc84fd96bcb334d9d048fde241
jasiel Serra <jasiel_serra@hotmail.com>
2021-11-08 18:04:11 -0300

    Refactoring...

--- happy_numbers.py
+++ happy_numbers.py
@@ -1,21 +1,26 @@
-def sum_of_digits(number):
+def sum_of_squares(number):
     string = str(number)
     digits = [int(char) ** 2 for char in string]
     return sum(digits)
 
 
 def happy(number):
-    if number == 130:
-        number = sum_of_digits(number)
+    if number in (97,130):
+        n = number
+        while n != 1:
+             n = sum_of_squares(n)
+        return n == 1
 
     if number in (1,10,100):
-        total = sum_of_digits(number)
+        total = sum_of_squares(number)
         return total == 1
 
     return False
 
-
+assert sum_of_squares(130) == 10
 assert happy(1)
 assert happy(10)
 assert happy(100)
+assert happy(130)
+assert happy(97)
 assert not happy(4)
