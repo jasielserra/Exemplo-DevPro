3939c8b8e0e4ca7d91011dd92f0c87b6c9c79a2c
jasiel Serra <jasiel_serra@hotmail.com>
2021-11-08 18:12:15 -0300

    Juntando tudo com a função all.

--- happy_numbers.py
+++ happy_numbers.py
@@ -5,22 +5,14 @@ def sum_of_squares(number):
 
 
 def happy(number):
-    if number in (97,130):
-        n = number
-        while n != 1:
-             n = sum_of_squares(n)
-        return n == 1
+    box = []
+    n = number
+    while n != 1 and n not in box:
+        box.append(n)
+        n = sum_of_squares(n)
+    return n == 1
 
-    if number in (1,10,100):
-        total = sum_of_squares(number)
-        return total == 1
-
-    return False
 
 assert sum_of_squares(130) == 10
-assert happy(1)
-assert happy(10)
-assert happy(100)
-assert happy(130)
-assert happy(97)
+assert all(happy(n) for n in (1, 10, 100, 130, 97))
 assert not happy(4)
