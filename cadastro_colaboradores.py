eeb97756c22265d3a24f32bed0c587414c8dc0fa
jasiel Serra <jasiel_serra@hotmail.com>
2022-01-09 23:10:17 -0300

    Utilizando Docs Strings

---
+++ cadastro_colaboradores.py
@@ -0,0 +1,12 @@
+"""
+Utilizando o conceito de dicionários, faça uma ficha de cadastro de 4 funcionários utilizando a seguinte estrutura:
+Chave: Nome Dados: Idade, email, setor, salario
+inclua os funcionários:
+
+João Pereira, 25, joao.pereira@hurb.com, marketing, 1950
+Maria Silva, 23, maria.silva@hurb.com, comercial, 2300
+Pedro Peixoto, 32, pedro.peixoto@hurb.com, operacao, 2625
+Luiza Almeida, 28, luiza.almeida@hurb.com, atendimento, 2120
+Faça um programa que retorna o nome, email e setor de todos os funcionários com de 25 anos
+"""
+
