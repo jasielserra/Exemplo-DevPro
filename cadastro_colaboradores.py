737ca8b32c7daaa5a0369d17b3cad91efb12ef17
jasiel Serra <jasiel_serra@hotmail.com>
2022-01-09 23:23:04 -0300

    Implementado casos de testes utilizando o unittest.

--- cadastro_colaboradores.py
+++ cadastro_colaboradores.py
@@ -9,4 +9,14 @@ Pedro Peixoto, 32, pedro.peixoto@hurb.com, operacao, 2625
 Luiza Almeida, 28, luiza.almeida@hurb.com, atendimento, 2120
 Faça um programa que retorna o nome, email e setor de todos os funcionários com de 25 anos
 """
+import unittest
 
+class TesteColaboradores(unittest.TestCase):
+    def test_filtragem_colaboradores(self) -> None:
+        self.assertFalse(True)
+     #   self.colaboradores = {
+     #       'joao pereira': [25, 'joao.pereira@hurb.com', 'marketing', 1950],
+     #       'Maria Silva' : [23, 'maria.silva@hurb.com', 'comercial', 2300],
+     #       'Pedro Peixoto' : [32, 'pedro.peixoto@hurb.com', 'operacao', 2625],
+     #       'Luiza Almeida' : [28, 'luiza.almeida@hurb.com', 'atendimento', 2120],
+     #   }
