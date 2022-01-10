76de33487f435dce7e23aedd6fb09aac554b0a73
jasiel Serra <jasiel_serra@hotmail.com>
2022-01-10 00:03:19 -0300

    Iterando sobre meu dicionario de colaboradores efetuando a verificação e retornando em uma tupla.

--- cadastro_colaboradores.py
+++ cadastro_colaboradores.py
@@ -13,13 +13,16 @@ import unittest
 
 
 def filtrar_maiores_de_25(colaboradores):
-    pass
+    resultado = []
+    for nome,(idade, email, setor,_) in colaboradores.items():
+        resultado.append((nome, email, setor))
+    return resultado
 
 
 class TesteColaboradores(unittest.TestCase):
     def test_filtragem_colaboradores(self) -> None:
         colaboradores = {
-            'joao pereira': [25, 'joao.pereira@hurb.com', 'marketing', 1950],
+            'Joao Pereira': [25, 'joao.pereira@hurb.com', 'marketing', 1950],
             'Maria Silva' : [23, 'maria.silva@hurb.com', 'comercial', 2300],
             'Pedro Peixoto' : [32, 'pedro.peixoto@hurb.com', 'operacao', 2625],
             'Luiza Almeida' : [28, 'luiza.almeida@hurb.com', 'atendimento', 2120],
