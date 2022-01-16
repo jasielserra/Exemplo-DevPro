859af532b26acbce0c3a62d12f57d9546d10b70a
jasiel Serra <jasiel_serra@hotmail.com>
2022-01-15 21:22:00 -0300

    Criando uma função de gerenciador de contexto.

--- matematica/gerenciador_de_contexto/contexto.py
+++ matematica/gerenciador_de_contexto/contexto.py
@@ -3,16 +3,28 @@ Gerenciador de contexto é um objeto que possui o metodo __enter__
 e __exit__.
 '''
 from contextlib import contextmanager
+from typing import Callable
+
+
+def contextmanager(chamavel: Callable):
+    class GerenciadorDeContexto:
+        def __enter__(self):
+            print('Entrada')
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            print('Saída')
+
+    return GerenciadorDeContexto
 
 
 @contextmanager
 def Arquivo():
     print(f'Enter: ')
-    return 'foo'
+    yield 'foo'
     print('Saída')
 
 
 
 with Arquivo() as arquivo: # Contruindo um objeto do tipo arquivo chamando o metodo __enter__ e retornando.
     #raise Exception
+    print(arquivo)
     print(id(arquivo))
