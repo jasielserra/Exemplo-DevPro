e3b9af245b73e97b3390288cb807453b3d5a723d
jasiel Serra <jasiel_serra@hotmail.com>
2022-01-15 21:03:56 -0300

    Imprimindo o id do objeto gerenciador de contexto.

--- matematica/gerenciador_de_contexto/contexto.py
+++ matematica/gerenciador_de_contexto/contexto.py
@@ -1,14 +1,21 @@
+'''
+Gerenciador de contexto é um objeto que possui o metodo __enter__
+e __exit__.
+'''
+
+
 class Arquivo:
     def __enter__(self):
         print(f'Enter: {id(self)}')
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
+        print('Saida', exc_type, exc_val,exc_tb)
 
 
 
-arquivo = Arquivo()
 
-with Arquivo() as arquivo:
+
+with Arquivo() as arquivo: # Contruindo um objeto do tipo arquivo chamando o metodo __enter__ e retornando.
+    #raise Exception
     print(id(arquivo))
